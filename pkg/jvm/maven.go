52531c9809d8887b01add22f3ac9bc62fe07a43d
J-K. Solbakken <jksolbakken@gmail.com>
2021-10-21 14:07:58 +0200

    understand maven output

---
+++ pkg/jvm/maven.go
@@ -0,0 +1,22 @@
+package jvm
+
+import (
+	"errors"
+	"regexp"
+	"strings"
+)
+
+func MavenCompileTimeDeps(mvnOutput string) (map[string] string, error) {
+	pattern := regexp.MustCompile(`(?m)\s{4}[a-zA-Z0-9.]+:.*`)
+	matches := pattern.FindAllString(mvnOutput, -1)
+	if matches == nil { return nil, errors.New("unable to find any dependencies")}
+
+	dependencies := make(map[string] string)
+	for _, match := range matches {
+		elements := strings.Split(strings.TrimSpace(match), ":")
+		name := elements[0] + ":" + elements[1]
+		dependencies[name] = elements[3]
+	}
+
+	return dependencies, nil
+}
